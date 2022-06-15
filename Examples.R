4d7e1bb8e1b482ae46a68e60e315a1d64d56ad84
Guillaume Guénard <guillaume.guenard@gmail.com>
2022-06-15 10:20:20 -0400

    Abaandonned the Mahal function development

--- Examples.R
+++ Examples.R
@@ -5,18 +5,3 @@
 ##
 library(codep)
 ##
-compile <- function() {
-  try(dyn.unload("./src/Mahal.so"),silent=TRUE)
-  system("R CMD SHLIB ./src/Mahal.c")
-  dyn.load("./src/Mahal.so")
-  source("./R/Mahal.R")
-}
-##
-compile()
-##
-x <- matrix(rnorm(30,5,2),10,3)
-cov <- matrix(c(1,2,3,2,1.5,3,3,2,0.5),3,3)
-## solve(cov)
-Mahal(x, cov = cov)
-dist(x)
-##
---
+++ Old/Examples.R
@@ -0,0 +1,23 @@
+##
+### codep package examples
+##
+## rm(list=ls())
+##
+library(codep)
+##
+compile <- function() {
+  try(dyn.unload("./src/Mahal.so"),silent=TRUE)
+  system("R CMD SHLIB ./src/Mahal.c")
+  dyn.load("./src/Mahal.so")
+  source("./R/Mahal.R")
+}
+##
+compile()
+##
+x <- matrix(rnorm(30,5,2),10,3)
+cov <- matrix(c(1,2,3,2,1.5,2,3,2,0.5),3,3)  ## solve(cov)  ## det(cov)
+## cov <- diag(3)
+## solve(cov)
+Mahal(x, cov = cov)
+dist(x)
+##
